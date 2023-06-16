3f823ec66de6a0d9c8ced4d4f042693fdc902e61
PP6464 <panthhp@outlook.com>
2023-06-16 17:42:20 +0100

    add image

--- src/main/kotlin/pages/Home.kt
+++ src/main/kotlin/pages/Home.kt
@@ -3,9 +3,20 @@ package pages
 import react.FC
 import react.Props
 import react.dom.html.ReactHTML.h1
+import react.dom.html.ReactHTML.div
+import react.dom.html.ReactHTML.img
+import web.cssom.ClassName
 
 val Home = FC<Props> {
-    h1 {
-        +"Home"
+    div {
+        className = ClassName("container")
+        img {
+            src = "/panth-pic.png"
+            alt = ""
+            id = "home-panth-pic"
+        }
+        h1 {
+            +"Home"
+        }
     }
 }
