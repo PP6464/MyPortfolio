5fcc10de09b062df08fae77ef9f586469aac56b4
Panth Patel <panthhp@outlook.com>
2023-06-22 19:39:48 +0100

    add background to page

--- src/main/kotlin/pages/Home.kt
+++ src/main/kotlin/pages/Home.kt
@@ -1,22 +1,39 @@
 package pages
 
+import js.core.jso
 import react.FC
 import react.Props
 import react.dom.html.ReactHTML.h1
 import react.dom.html.ReactHTML.div
 import react.dom.html.ReactHTML.img
+import react.dom.html.ReactHTML.p
 import web.cssom.ClassName
+import web.cssom.TextAlign
 
 val Home = FC<Props> {
 	div {
 		className = ClassName("container")
 		img {
+			src = "/code-background.png"
+			alt = ""
+			className = ClassName("code-background")
+		}
+		div {
+			className = ClassName("white-background")
+		}
+		img {
 			src = "/panth-pic.png"
 			alt = ""
-			id = "home-panth-pic"
+			className = ClassName("panth-pic")
 		}
 		h1 {
-			+"Home"
+			+"Panth Patel"
+		}
+		p {
+			style = jso {
+				textAlign = TextAlign.center
+			}
+			+"I am a programmer with experience in many areas and frameworks"
 		}
 	}
 }
