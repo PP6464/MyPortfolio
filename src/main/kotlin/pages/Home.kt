e653e34ac7f17121f68cd63b8057438ab859fed9
Panth Patel <panthhp@outlook.com>
2023-06-24 10:21:29 +0100

    animate content of home page

--- src/main/kotlin/pages/Home.kt
+++ src/main/kotlin/pages/Home.kt
@@ -7,10 +7,29 @@ import react.dom.html.ReactHTML.h1
 import react.dom.html.ReactHTML.div
 import react.dom.html.ReactHTML.img
 import react.dom.html.ReactHTML.p
+import react.useEffect
 import web.cssom.ClassName
+import web.cssom.Padding
 import web.cssom.TextAlign
+import web.cssom.px
+import web.dom.document
+import web.dom.observers.IntersectionObserver
 
 val Home = FC<Props> {
+	val observer = IntersectionObserver(callback = { entries, _ ->
+		entries.map {
+			if (it.isIntersecting) {
+				it.target.classList.add("visible")
+			} else {
+				it.target.classList.remove("visible")
+			}
+		}
+	})
+	
+	useEffect {
+		val sections = document.querySelectorAll(".section")
+		sections.forEach(observer::observe)
+	}
 	div {
 		className = ClassName("container")
 		img {
@@ -32,8 +51,57 @@ val Home = FC<Props> {
 		p {
 			style = jso {
 				textAlign = TextAlign.center
+				padding = Padding(0.px, 10.px, 5.px)
 			}
-			+"I am a programmer with experience in many areas and frameworks"
+			+"I am a self-taught programmer with experience in many areas and frameworks."
+		}
+		div {
+			className = ClassName("section")
+			div {
+				h1 {
+					+"Areas of expertise"
+				}
+			}
+			img {
+				src = "/code-background.png"
+				alt = ""
+			}
+		}
+		div {
+			className = ClassName("section")
+			div {
+				h1 {
+					+"Frameworks"
+				}
+			}
+			img {
+				src = "/code-background.png"
+				alt = ""
+			}
+		}
+		div {
+			className = ClassName("section")
+			div {
+				h1 {
+					+"What I'm exploring"
+				}
+			}
+			img {
+				src = "/code-background.png"
+				alt = ""
+			}
+		}
+		div {
+			className = ClassName("section")
+			div {
+				h1 {
+					+"My Code"
+				}
+			}
+			img {
+				src = "/code-background.png"
+				alt = ""
+			}
 		}
 	}
 }
