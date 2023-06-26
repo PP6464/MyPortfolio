022e00b499157eba597f2805ca72aaf7dff226a7
Panth Patel <panthhp@outlook.com>
2023-06-26 17:49:30 +0100

    center homepage text when in desktop and underline links

--- src/main/kotlin/pages/Home.kt
+++ src/main/kotlin/pages/Home.kt
@@ -57,11 +57,16 @@ val Home = FC<Props> {
 		div {
 			className = ClassName("section")
 			div {
-				h1 {
-					+"Areas of expertise"
-				}
-				p {
-					+"I have expertise in many areas of programming, and I am a full stack developer. I am able to develop both frontend and backend with many frameworks, making me a great fit into most projects."
+				div {
+					h1 {
+						+"Areas of expertise"
+					}
+					p {
+						style = jso {
+							paddingBottom = 45.px
+						}
+						+"I have expertise in many areas of programming, and I am a full stack developer. I am able to develop both frontend and backend with many frameworks, making me a great fit into most projects."
+					}
 				}
 				Link {
 					style = jso {
@@ -87,11 +92,16 @@ val Home = FC<Props> {
 		div {
 			className = ClassName("section")
 			div {
-				h1 {
-					+"Frameworks"
-				}
-				p {
-					+"I am adept with many programming frameworks for both frontend and backend development, so I can fill many roles in many different projects."
+				div {
+					h1 {
+						+"Frameworks"
+					}
+					p {
+						style = jso {
+							paddingBottom = 45.px
+						}
+						+"I am adept with many programming frameworks for both frontend and backend development, so I can fill many roles in many different projects."
+					}
 				}
 				Link {
 					style = jso {
@@ -117,11 +127,16 @@ val Home = FC<Props> {
 		div {
 			className = ClassName("section")
 			div {
-				h1 {
-					+"What I'm exploring"
-				}
-				p {
-					+"I am always looking to expand my skill set, to make me more suitable for more projects. I am currently looking to expand my capabilities in game development and low level programming as well as further enhancing my skills in my areas of expertise with new frameworks."
+				div {
+					h1 {
+						+"What I'm exploring"
+					}
+					p {
+						style = jso {
+							paddingBottom = 45.px
+						}
+						+"I am always looking to expand my skill set, to make me more suitable for more projects. I am currently looking to expand my capabilities in game development and low level programming as well as further enhancing my skills in my areas of expertise with new frameworks."
+					}
 				}
 				Link {
 					style = jso {
@@ -147,11 +162,16 @@ val Home = FC<Props> {
 		div {
 			className = ClassName("section")
 			div {
-				h1 {
-					+"My Code"
-				}
-				p {
-					+"I believe in collaboration when it comes to programming, and so I use Github to host my code and collaborate with others. Please click below to view my code."
+				div {
+					h1 {
+						+"My Code"
+					}
+					p {
+						style = jso {
+							paddingBottom = 45.px
+						}
+						+"I believe in collaboration when it comes to programming, and so I use Github to host my code and collaborate with others. Please click below to view my code."
+					}
 				}
 				Link {
 					style = jso {
