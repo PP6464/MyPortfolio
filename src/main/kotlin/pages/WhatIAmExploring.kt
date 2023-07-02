91cd036b302291ab335df1ad8ebacc33f8550ed2
Panth Patel <panthhp@outlook.com>
2023-07-02 12:30:18 +0100

    Add basic content

--- src/main/kotlin/pages/WhatIAmExploring.kt
+++ src/main/kotlin/pages/WhatIAmExploring.kt
@@ -1,11 +1,15 @@
 package pages
 
+import js.core.jso
+import mui.icons.material.ArrowOutward
 import react.FC
 import react.Props
 import react.dom.html.ReactHTML.h1
 import react.dom.html.ReactHTML.div
 import react.dom.html.ReactHTML.img
-import web.cssom.ClassName
+import react.dom.html.ReactHTML.p
+import react.router.dom.Link
+import web.cssom.*
 
 val WhatIAmExploring = FC<Props> {
 	div {
@@ -25,6 +29,146 @@ val WhatIAmExploring = FC<Props> {
 		}
 		h1 {
 			+"What I'm exploring"
+		}
+		div {
+			className = ClassName("section")
+			div {
+				div {
+					h1 {
+						+"Game development"
+					}
+					p {
+						style = jso {
+							paddingBottom = 45.px
+						}
+						+"I have expertise in many areas of programming, and I am a full stack developer. I am able to develop both frontend and backend with many frameworks, making me a great fit into most projects."
+					}
+				}
+				Link {
+					style = jso {
+						display = Display.flex
+						alignItems = AlignItems.center
+						justifyContent = JustifyContent.center
+						fontSize = 15.px
+					}
+					to = "/frameworks#web-development"
+					+"Learn more"
+					ArrowOutward {
+						style = jso {
+							height = 17.5.px
+						}
+					}
+				}
+			}
+			img {
+				src = "/code-background.png"
+				alt = ""
+			}
+		}
+		div {
+			className = ClassName("section")
+			div {
+				div {
+					h1 {
+						+"Computer Vision"
+					}
+					p {
+						style = jso {
+							paddingBottom = 45.px
+						}
+						+"I have expertise in many areas of programming, and I am a full stack developer. I am able to develop both frontend and backend with many frameworks, making me a great fit into most projects."
+					}
+				}
+				Link {
+					style = jso {
+						display = Display.flex
+						alignItems = AlignItems.center
+						justifyContent = JustifyContent.center
+						fontSize = 15.px
+					}
+					to = "/frameworks#web-development"
+					+"Learn more"
+					ArrowOutward {
+						style = jso {
+							height = 17.5.px
+						}
+					}
+				}
+			}
+			img {
+				src = "/code-background.png"
+				alt = ""
+			}
+		}
+		div {
+			className = ClassName("section")
+			div {
+				div {
+					h1 {
+						+"Angular"
+					}
+					p {
+						style = jso {
+							paddingBottom = 45.px
+						}
+						+"I have expertise in many areas of programming, and I am a full stack developer. I am able to develop both frontend and backend with many frameworks, making me a great fit into most projects."
+					}
+				}
+				Link {
+					style = jso {
+						display = Display.flex
+						alignItems = AlignItems.center
+						justifyContent = JustifyContent.center
+						fontSize = 15.px
+					}
+					to = "/frameworks#web-development"
+					+"Learn more"
+					ArrowOutward {
+						style = jso {
+							height = 17.5.px
+						}
+					}
+				}
+			}
+			img {
+				src = "/code-background.png"
+				alt = ""
+			}
+		}
+		div {
+			className = ClassName("section")
+			div {
+				div {
+					h1 {
+						+"Arduino"
+					}
+					p {
+						style = jso {
+							paddingBottom = 45.px
+						}
+						+"I have expertise in many areas of programming, and I am a full stack developer. I am able to develop both frontend and backend with many frameworks, making me a great fit into most projects."
+					}
+				}
+				Link {
+					style = jso {
+						display = Display.flex
+						alignItems = AlignItems.center
+						justifyContent = JustifyContent.center
+						fontSize = 15.px
+					}
+					to = "/frameworks#web-development"
+					+"Learn more"
+					ArrowOutward {
+						style = jso {
+							height = 17.5.px
+						}
+					}
+				}
+			}
+			img {
+				src = "/code-background.png"
+				alt = ""
+			}
 		}
 	}
 }
